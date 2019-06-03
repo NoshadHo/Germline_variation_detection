f6bdd11b40589cdc265fcdb727424e80c33dee9b
noshadho <noshadho@gmail.com>
2019-06-03 16:00:33 -0400

    box_plot ready foor only one tile

--- tiles_correlation_analysis.R
+++ tiles_correlation_analysis.R
@@ -17,9 +17,12 @@
     library(mclust)
     library(gridExtra)
     library(corrr)
+    library(plotly)
     set.seed(1024)
 
   #Data load:
+    #base::load('/home/noshadh/Codes/Germline_variation_detection/K-means_Multimodal_fitting_data.RData')
+    #tile_case = t(tile_case)
   #we have saved it in tile_case_comparison.R file, we just load it from there
     tile_case = read_tsv('/home/noshadh/Codes/Germline_variation_detection/Tile_case_hl5.tsv',col_names = TRUE)
   #Add tile number as the first column
@@ -28,6 +31,8 @@
   
   #load the selected tiles from tile_Case_comparison.R file
     vol_tiles = read_tsv('/home/noshadh/Codes/Germline_variation_detection/Selected_volatile_tiles.tsv',col_names = TRUE)
+    #vol_tiles = significant_tiles
+    
   #join it with tile case to capture ncov:
     vol_tiles = vol_tiles %>% select(tile) #only select the ids
     vol_tiles = vol_tiles %>% left_join(tile_case,by = "tile")
@@ -35,14 +40,42 @@
 #CAPTURING THE CORRELATION COEFFICIENT----------------------------------------------------------------------------------------------------------
   #---FIrst we will try our approach on the selected tiles from tile_Case_comparison.R file---
   #predefinning the data.frame
-    tile_tile_corr_selected = data.frame(matrix(nrow = dim(vol_tiles)[1],ncol = dim(vol_tiles)[1]))
+    tile_tile_corr_selected = (matrix(nrow = dim(vol_tiles)[1],ncol = dim(vol_tiles)[1]))
   #calculating the correlation (this function calculate between columns, so we transpose the data.frame)
     tile_tile_corr_selected = cor(t(vol_tiles))
-    tile_tile_corr_selected = as.data.frame(tile_tile_corr_selected)
-  #write the file
-    write_tsv(tile_tile_corr_selected, "/home/noshadh/Codes/Germline_variation_detection/selected_tiles_pairwise_correlation.tsv")
+    tile_tile_corr_selected = as.data.frame(tile_tile_corr_selected) #this step is not neccessary
+    
+    #make the correlations under 0 to it's absoloute value
+    tile_tile_corr_selected_pos = abs(tile_tile_corr_selected)
+#PLOTS FOR  ONE TILE ------------------------------------------------------------------------------------------------------------------------------------------
+  #a simple plot for a arbitary tile, to look at it's correlation with other tiles
+    data_file = tile_tile_corr_selected_pos #to work with positiive/negetive file more easily
+    tile1_corr = data_file[1000,]
+    tile_nums = 1:dim(data_file)[1]
+    tile1_corr = rbind(tile1_corr,tile_nums)
+    #group points for each 10mb
+    tile1_corr = rbind(tile1_corr,tile_nums)
+    
+    group = 1
+    for (i in 1:dim(data_file)[1]){
+      if (i > group*1000){
+        group = group + 1
+      }
+      tile1_corr[3,i] = (group)
+      
+      print(i)
+    }
+    tile1_corr[3,] = as.factor(tile1_corr[3,])
+    
+    df = as.data.frame(t(tile1_corr[,1:10000]))
+    colnames(df) = c('value','index','group')
+    p = df %>% ggplot(aes(x = group, y = value, fill = group)) +
+        geom_boxplot(na.rm = TRUE) + theme(
+          axis.text.y = element_blank(),
+          axis.ticks = element_blank())
+    ggplotly(p)
 
-#10MB BOX-PLOTS ANALYSIS-------------------------------------------------------------------------------------------------------------------------
+    #10MB BOX-PLOTS ANALYSIS------------------------------------------------------------------------------------------------------------------------
 #IN THIS PART, WE MEASURE EACH SELECTED TILE CORRELATION WITH:
 #GROUP 1: ALL THE TILES IN THE DISTANCE OF <10MB
 #GROUP 2: ALL THE TILES IN THE DISTANCE OF >10MB AND <20MB
