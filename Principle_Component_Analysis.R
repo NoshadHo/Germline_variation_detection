5d8e66640c77b2b575b8b46a9271759cab331925
noshadho <noshadho@gmail.com>
2019-06-13 19:10:14 -0400

    row_sum and col_sum analysis added to the PCA code

--- Principle_Component_Analysis.R
+++ Principle_Component_Analysis.R
@@ -41,16 +41,26 @@
     pc_variance %>% ggplot()+
       geom_point(aes(y = variance, x = 1:dim(pc_variance)[1]))+theme_minimal()
   #By looking at these values, I decided to remove three of PC's
-    num_PC = 1
+    num_PC = 3
     
 #REMOVING THE COMPONENTS FROM NORMAL DATA-------------------------------------------------------------------------------------------------------
   #first method: find eigen vslues, rotate data to that space, make the value zero, rotate back
     #eigen_vectors = eigen(cov(tile_cov_gc_normalized))$vectors
     eigen_vectors = prcomp(tile_cov_gc_normalized, center = TRUE)$rotation
     rotated_tile_cov_gc_normalized = as.matrix(tile_cov_gc_normalized) %*% eigen_vectors
-    rotated_tile_cov_gc_normalized[, 1:num_PC] <- 0
-    purified_tile_cov_gc_normalized <- rotated_tile_cov_gc_normalized %*% t(eigen_vectors) # transpose of orthogonal matrix = inverse
+    rotated_tile_cov_gc_normalized[, 1:num_PC] = 0
+    purified_tile_cov_gc_normalized = rotated_tile_cov_gc_normalized %*% t(eigen_vectors) # transpose of orthogonal matrix = inverse
     
+  #(OPTIONAL) Analysing the effect of principle reduction
+    sum(purified_tile_cov_gc_normalized)
+    #analysing column changes from pc1:3 removed compare to pc1:4 removed (expect to be pseudo uniformily)
+    col_sum3 = as.data.frame(purified_tile_cov_gc_normalized) %>% summarise_all(funs(sum))  #sum of columns #change num_pc to 4 and try again for col_sum3
+    col_changes34 = abs(col_sum3) - abs(col_sum4)                                           #sum of changes from removing pc4 from pc3removed case
+    ggplot() + geom_point(aes(x = 1:dim(col_changes34)[2],y = t(col_changes34)))
+    #analysing row changes from pc1:3 removed compare to pc1:4 removed (expect to be non-uniformly)
+    row_sum3 = rowSums(x = purified_tile_cov_gc_normalized, dims = 1)
+    row_changes34 = abs(row_sum3) - abs(row_sum4) 
+    ggplot() + geom_point(aes(x = 1:length(row_changes34),y = (row_changes34)))+theme_minimal()+ylim(0,1)
   #QC: turn the space into pc subspace and plot
     QC_matrix = as.matrix(purified_tile_cov_gc_normalized) %*% eigen_vectors
     ggplot()+geom_point(aes(x = QC_matrix[,1], y = QC_matrix[,2]))
