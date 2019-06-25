428a3d16ebb5e472367615b460fec704b40d07b4
noshadho <noshadho@gmail.com>
2019-06-25 12:05:31 -0400

    add coordinates to tile function, in order to read the .BED files

--- tile_to_coordinates.R
+++ tile_to_coordinates.R
@@ -4,6 +4,15 @@ tile_to_coordinates = function(tile,file){ #file is a one of the .RDS files of p
 }
 
 
-coordinates_to_tile = function(chr,start,end,file){
-    
+coordinates_to_tile = function(chr,start_coord,end_coord,file){
+  range = as.data.frame(file$tile)
+  range = range %>% mutate(tile = row_number())
+  start_tile = (range %>% filter(start < start_coord & end > start_coord & seqnames == as.character(chr)))$tile
+  end_tile = (range %>% filter(start < end_coord & end > end_coord & seqnames == as.character(chr)))$tile
+  output = as.data.frame(matrix(nrow = 1,ncol = 2))
+  output[,1] = start_tile
+  output[,2] = end_tile
+  colnames(output) = c('start_tile','end_tile')
+  return(output)
 }
+
