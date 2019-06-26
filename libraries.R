d8b9122ec3c570a58c21fe782bd3c63523bce40e
noshadho <noshadho@gmail.com>
2019-06-26 17:42:07 -0400

    add a libraries file, so all the requiered libraries would be in it

---
+++ libraries.R
@@ -0,0 +1,25 @@
+library(GenomicRanges)
+library(cnvex) #this is a local library, might not work on other machines
+library(tidyverse)
+library(rlist)
+library(gplots)
+library(cluster)    # clustering algorithms
+library(factoextra) # clustering algorithms & visualization
+library(mclust)
+library(gridExtra)
+library(corrr)
+library(plotly)
+library(rlang)
+library(limma) #bioconductor lib
+library(gridExtra)
+library(ggfortify)
+library(mclust)
+library(foreach)
+library(doParallel)
+library(Rcpp)
+library(fpc)
+library(dbscan)
+set.seed(1024)
+numCores = detectCores()
+registerDoParallel(numCores-1)
+set.seed(1024)
