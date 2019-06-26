c02557b4bb32ced335199a7eb4a994c2d0d1b53b
noshadho <noshadho@gmail.com>
2019-06-26 19:47:22 -0400

    final commit of the day :)

--- repeart_annotation.R
+++ repeart_annotation.R
@@ -14,13 +14,13 @@ bed_to_tile_modified = function(bed_file,col_names = FALSE, RDSfile){
   tiles = list()
   
   PTIME = system.time({
-    tiles = foreach(i = 1:5000) %dopar% {
+    tiles = foreach(i = 1:dim(bed_file)[1]) %dopar% {
       return(c((coordinates_to_tile(bed_file$chr[i], bed_file$start[i],bed_file$end[i],file))))
   }
   })
   tiles = as.data.frame(do.call(rbind,tiles))
   colnames(tiles) = c("start_tile","end_tiles")
-  tiles = tiles %>% mutate(repeat_class = repeat_file_short$repeat_class[1:499])
+  tiles = tiles %>% mutate(repeat_class = repeat_file_short$repeat_class)
   #if a region contains more than one tile, make it to a couple of entry, each containing one tile
   more_than_one_tile = tiles %>% filter(start_tile != end_tiles)
   tiles_single = tiles %>% anti_join(more_than_one_tile)
